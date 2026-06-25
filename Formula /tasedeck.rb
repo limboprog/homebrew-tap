befffe01df5d0cd59d8c872393ed1e07419cc7e8
limboprog <borodin.leon07@gmail.com>
2026-06-25 10:56:54 +0700

    Create tasedeck.rb

---
+++ Formula /tasedeck.rb
@@ -0,0 +1,21 @@
+class Tasedeck < Formula
+  desc "Desktop client for managing MCP servers and developer tools"
+  homepage "https://github.com/ТВОЙ_ЛОГИН_НА_GITHUB/tasedeck"
+  version "0.1.0-alpha"
+
+  if Hardware::CPU.intel?
+    url "https://github.com/ТВОЙ_ЛОГИН_НА_GITHUB/tasedeck/releases/download/v0.1.0-alpha/TaseDeck_0.1.0_x64.dmg"
+    sha256 "sha256:02af637bfb06ac0cf07163b39ad9e1d5526cf57fa8f2c846d97161d1c84b1efb"
+  elsif Hardware::CPU.arm?
+    url "https://github.com/ТВОЙ_ЛОГИН_НА_GITHUB/tasedeck/releases/download/v0.1.0-alpha/TaseDeck_0.1.0_aarch64.dmg"
+    sha256 "sha256:15a48788bc4cae2e4580f59045a147c74b034bc0cbbf88f6518784f002c7d7cc"
+  end
+    prefix.install "TaseDeck.app" 
+  end
+
+  def caveats
+    <<~EOS
+      TaseDeck was successfully installed into your Applications folder!
+    EOS
+  end
+end
