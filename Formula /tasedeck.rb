fb7e786a6dacc7647e970762559406f5c8a27b3f
limboprog <borodin.leon07@gmail.com>
2026-06-25 11:09:08 +0700

    Update tasedeck.rb

--- Formula /tasedeck.rb
+++ Formula /tasedeck.rb
@@ -5,10 +5,10 @@ class Tasedeck < Formula
 
   if Hardware::CPU.intel?
     url "https://github.com/limboprog/tasedeck/releases/download/v0.1.0-alpha/TaseDeck_0.1.0_x64.dmg"
-    sha256 "sha256:02af637bfb06ac0cf07163b39ad9e1d5526cf57fa8f2c846d97161d1c84b1efb"
+    sha256 "02af637bfb06ac0cf07163b39ad9e1d5526cf57fa8f2c846d97161d1c84b1efb"
   elsif Hardware::CPU.arm?
     url "https://github.com/limboprog/tasedeck/releases/download/v0.1.0-alpha/TaseDeck_0.1.0_aarch64.dmg"
-    sha256 "sha256:15a48788bc4cae2e4580f59045a147c74b034bc0cbbf88f6518784f002c7d7cc"
+    sha256 "15a48788bc4cae2e4580f59045a147c74b034bc0cbbf88f6518784f002c7d7cc"
   end
     prefix.install "TaseDeck.app" 
   end
